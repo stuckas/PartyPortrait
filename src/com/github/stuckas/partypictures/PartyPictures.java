8ce1dbee2026980c89066e0c2a07c367c264fd3f
stuckas <stuckas@gmx.ch>
2012-01-23 21:38:44 +0100

    simplified event handler

--- src/com/github/stuckas/partypictures/PartyPictures.java
+++ src/com/github/stuckas/partypictures/PartyPictures.java
@@ -6,10 +6,10 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
@@ -105,22 +105,14 @@ public class PartyPictures extends JFrame {
 		
 	}
 	
-	private class ExitOnMouseClickListener implements MouseListener {
+	private class ExitOnMouseClickListener extends MouseAdapter {
 
 		public void mouseClicked(MouseEvent e) {
 			System.exit(0);
 		}
-
-		public void mousePressed(MouseEvent e) {}
-
-		public void mouseReleased(MouseEvent e) {}
-
-		public void mouseEntered(MouseEvent e) {}
-
-		public void mouseExited(MouseEvent e) {}
 	}
 	
-	private class TakePhotoOnKeyListener implements KeyListener {
+	private class TakePhotoOnKeyListener extends KeyAdapter {
 		
 		public void keyPressed(KeyEvent e) {
 			
@@ -160,9 +152,5 @@ public class PartyPictures extends JFrame {
 				}
 			}).start();
 		}
-
-		public void keyReleased(KeyEvent e) {}
-
-		public void keyTyped(KeyEvent e) {}
 	}
 }
