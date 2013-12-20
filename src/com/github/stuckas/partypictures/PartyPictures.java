0b822350fc3e35705c03af15df33183ea1f41da8
lusu <lusu@logobject.ch>
2013-12-20 22:33:31 +0100

    random order over all pictures and positions

--- src/com/github/stuckas/partypictures/PartyPictures.java
+++ src/com/github/stuckas/partypictures/PartyPictures.java
@@ -14,143 +14,175 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
 public class PartyPictures extends JFrame {
-	
-	static final long serialVersionUID = 1;
-	
+
+        static final long serialVersionUID = 1;
+
     // hard coded config
-	private static boolean FULLSCREEN = true;
-	
-	private Image full = null;
-	private Image[][] saver = new Image[5][5];
-	private Random r = new Random(1);
-	
-	Timer saverTimer = new Timer(2000, new UpdateSaver());
-	
-	boolean running = false;
-	
+        private static boolean FULLSCREEN = true;
 
-	public static void main(String[] args) throws Exception {
-		new PartyPictures();
-	}
+        private Image full = null;
+        private Image[][] saver = new Image[3][3];
+        private String[] randomPictures = null;
+        private int[][] randomOrder = null;
+        private int nextPicture = 0, nextPosition = 0;
+        private Random r = new Random(1);
 
-	// Class constructor
-	private PartyPictures() throws MalformedURLException {
+        Timer saverTimer = new Timer(2000, new UpdateSaver());
 
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		addMouseListener(new ExitOnMouseClickListener());
-		addKeyListener(new TakePhotoOnKeyListener());
+        boolean running = false;
 
-		this.setUndecorated(true);
-		this.setVisible(true);
 
-		if (FULLSCREEN) {
-			 GraphicsEnvironment.getLocalGraphicsEnvironment()
-			 	.getDefaultScreenDevice().setFullScreenWindow(this);
-		} else {
-			this.setSize(300, 200);
-		}
-		
-		saverTimer.start();
-	}
+        public static void main(String[] args) throws Exception {
+                new PartyPictures();
+        }
 
-	public void paint(Graphics g) {
-		if (full != null) {
-			g.drawImage(full, 0, 0, this);
-		}
+        // Class constructor
+        private PartyPictures() throws MalformedURLException {
 
-		if (saver != null) {
-			int x=0, y=0;
-			for (int i=0; i<saver.length; i++) {
-				for (int j=0;j<saver[i].length; j++) {
-					g.drawImage(saver[i][j], x, y, this);
-					x=x+getWidth()/saver[i].length;
-				}
-				x=0;
-				y=y+getHeight()/saver.length;
-			}
-		}
-	}
+                setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                addMouseListener(new ExitOnMouseClickListener());
+                addKeyListener(new TakePhotoOnKeyListener());
 
-	
-	private class UpdateSaver implements ActionListener {
+                this.setUndecorated(true);
+                this.setVisible(true);
 
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			File f = new File(".");
-			String[] files = f.list(new FilenameFilter() {
-				
-				@Override
-				public boolean accept(File dir, String name) {
-					return name.endsWith(".JPG");
-				}
-			});
-			
-			String filename = files[r.nextInt(files.length)];
-			int x = r.nextInt(saver.length);
-			int y = r.nextInt(saver[x].length);
-			
-			Image img = Toolkit.getDefaultToolkit().getImage(filename);
+                if (FULLSCREEN) {
+                         GraphicsEnvironment.getLocalGraphicsEnvironment()
+                                .getDefaultScreenDevice().setFullScreenWindow(this);
+                } else {
+                        this.setSize(300, 200);
+                }
 
-			img = img.getScaledInstance(getWidth()/saver.length, getHeight()/saver[x].length, Image.SCALE_FAST);
-			saver[x][y] = img;
-			full = null;
-			repaint();
-		}
-		
-	}
-	
-	private class ExitOnMouseClickListener extends MouseAdapter {
+                // init random order
+                randomOrder = new int[saver.length*saver[0].length][2];
+                for (int i=0; i<saver.length; i++) {
+                	for (int j=0; j<saver[0].length; j++) {
+                		randomOrder[i*saver.length+j] = new int[] {i, j};
+                	}
+                }
+                
+                saverTimer.start();
+        }
 
-		public void mouseClicked(MouseEvent e) {
-			System.exit(0);
-		}
-	}
-	
-	private class TakePhotoOnKeyListener extends KeyAdapter {
-		
-		public void keyPressed(KeyEvent e) {
-			
-			if (running)
-				return;
-			
-			saverTimer.stop();
+        public void paint(Graphics g) {
+                if (full != null) {
+                        g.drawImage(full, 0, 0, this);
+                }
 
-			new Thread(new Runnable() {
-				public void run() {
-					running = true;
+                if (saver != null) {
+                        int x=0, y=0;
+                        for (int i=0; i<saver.length; i++) {
+                                for (int j=0;j<saver[i].length; j++) {
+                                        g.drawImage(saver[i][j], x, y, this);
+                                        x=x+getWidth()/saver[i].length;
+                                }
+                                x=0;
+                                y=y+getHeight()/saver.length;
+                        }
+                }
+        }
 
-					String filename = System.currentTimeMillis() + ".JPG";
-					try {
-						Process p = Runtime.getRuntime().exec(
-								"gphoto2 --capture-image-and-download --filename=" + filename);
-						p.waitFor();
-						full = Toolkit.getDefaultToolkit().getImage(filename);
-						full = full.getScaledInstance(getWidth(), -1, Image.SCALE_FAST);
-					} catch (IOException ex) {
-						ex.printStackTrace();
-					} catch (java.lang.InterruptedException ex) {
-					} finally {
-						running = false;
-						init(saver);
-						repaint();
-						saverTimer.start();
-					}
-				}
-				
-				public void init(Image[][] in) {
-					for (int i=0; i<in.length; i++) {
-						for (int j=0; j<in[i].length; j++) {
-							in[i][j] = null;
-						}
-					}
-				}
-			}).start();
-		}
-	}
+
+        private class UpdateSaver implements ActionListener {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                        
+                        if (randomPictures.length > 0) {
+                                String filename = randomPictures[nextPicture++];
+
+                                int[] pos = randomOrder[nextPosition++];
+                                int x = pos[0];
+                                int y = pos[1];
+                                
+                                if (nextPicture >= randomPictures.length)
+                                	nextPicture = 0;
+                                
+                                if (nextPosition >= randomOrder.length)
+                                	nextPosition = 0;
+
+                                Image img = Toolkit.getDefaultToolkit().getImage(filename);
+
+                                img = img.getScaledInstance(getWidth()/saver.length, getHeight()/saver[x].length, Image.SCALE_FAST);
+                                saver[x][y] = img;
+                                full = null;
+                                repaint();
+                        }
+                }
+
+        }
+
+        private class ExitOnMouseClickListener extends MouseAdapter {
+
+                public void mouseClicked(MouseEvent e) {
+                        System.exit(0);
+                }
+        }
+
+        private class TakePhotoOnKeyListener extends KeyAdapter {
+
+                public void keyReleased(KeyEvent e) {
+                        System.out.println("Key event received: "+e);
+                        if (running) {
+		                    return;
+                        }
+
+                        saverTimer.stop();
+
+                        new Thread(new Runnable() {
+                                public void run() {
+                                        running = true;
+
+                                        String filename = System.currentTimeMillis() + ".JPG";
+                                        System.out.println("Filename:"+filename);
+                                        try {
+                                                Process p = Runtime.getRuntime().exec(
+                                                                "gphoto2 --capture-image-and-download --filename=" + filename);
+                                                p.waitFor();
+                                                full = Toolkit.getDefaultToolkit().getImage(filename);
+                                                full = full.getScaledInstance(getWidth(), -1, Image.SCALE_FAST);
+                                        } catch (IOException ex) {
+                                                ex.printStackTrace();
+                                        } catch (java.lang.InterruptedException ex) {
+                                                System.out.println(ex.getMessage());
+                                        } finally {
+                                                running = false;
+                                                init(saver);
+                                                repaint();
+                                                
+                                                // randomize and initialize random files
+                                                File f = new File(".");
+                                                randomPictures = f.list(new FilenameFilter() {
+
+                                                        @Override
+                                                        public boolean accept(File dir, String name) {
+                                                                return name.endsWith(".JPG");
+                                                        }
+                                                });
+                                                
+                                                Collections.shuffle(Arrays.asList(randomPictures), r);
+                                                Collections.shuffle(Arrays.asList(randomOrder), r);
+                                                
+                                                saverTimer.start();
+                                        }
+                                }
+
+                                public void init(Image[][] in) {
+                                        for (int i=0; i<in.length; i++) {
+                                                for (int j=0; j<in[i].length; j++) {
+                                                        in[i][j] = null;
+                                                }
+                                        }
+                                }
+                        }).start();
+                }
+        }
 }
